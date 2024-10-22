3650bce3b21c217c7cd26016aa43aefd21f7c738
ren0 <renomg.194@gmail.com>
2024-10-22 14:11:28 -0600

    dev branch fixes

--- src/app/media/media.spec.ts
+++ src/app/media/media.spec.ts
@@ -1,13 +1,13 @@
 import { calculateMean } from './media';
 
 describe('calculateMean', () => {
-    it('should return the correct mean for column 1', () => {
+    it(' should return media = 550.6 if input is [160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503]', () => {
         const col1 = [160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503];
         const meanCol1 = calculateMean(col1);
         expect(meanCol1).toBeCloseTo(550.6, 1);
     });
 
-    it('should return the correct mean for column 2', () => {
+    it('should return media = 60.32 if input is [15, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]', () => {
         const col2 = [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2];
         const meanCol2 = calculateMean(col2);
         expect(meanCol2).toBeCloseTo(60.32, 2);
