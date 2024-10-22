f36ddabc422751b6ccd65578e38271415dca36f9
ren0 <renomg.194@gmail.com>
2024-10-22 00:58:05 -0600

    stddev

--- src/app/stddev/stddev.component.spec.ts
+++ src/app/stddev/stddev.component.spec.ts
@@ -12,23 +12,23 @@ describe("StddevComponent", () => {
 
     fixture = TestBed.createComponent(StddevComponent);
     component = fixture.componentInstance;
-    component.columna1 = [160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503];
-    component.columna2 = [
+    component.column1 = [160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503];
+    component.column2 = [
       15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2,
     ];
 
     component.ngOnInit();
   });
   it("should return stddev = 572.03 if input is [160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503] ", () => {
-    const expectedStddevColumna1 = 572.03;
+    const expectedStddevColumn1 = 572.03;
 
-    expect(component.stddevColumna1).toBe(expectedStddevColumna1);
+    expect(component.stddevColumn1).toBe(expectedStddevColumn1);
   });
 
   it("should return stddev = 62.26 if input is [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2] ", () => {
-    const expectedStddevColumna2 = 62.26;
+    const expectedStddevColumn2 = 62.26;
 
-    expect(component.stddevColumna2).toBe(expectedStddevColumna2);
+    expect(component.stddevColumn2).toBe(expectedStddevColumn2);
   });
   it("should create", () => {
     expect(component).toBeTruthy();
--- src/app/stddev/stddev.component.ts
+++ src/app/stddev/stddev.component.ts
@@ -10,54 +10,54 @@ import cols from "./data.json";
   styleUrl: "./stddev.component.css",
 })
 export class StddevComponent implements OnInit {
-  columna1: number[] = cols.columna1;
-  columna2: number[] = cols.columna2;
-  mediaColumna1: number = 0;
-  mediaColumna2: number = 0;
-  stddevColumna1: number = 0;
-  stddevColumna2: number = 0;
+  column1: number[] = cols.column1;
+  column2: number[] = cols.column2;
+  mediaColumn1: number = 0;
+  mediaColumn2: number = 0;
+  stddevColumn1: number = 0;
+  stddevColumn2: number = 0;
 
   ngOnInit(): void {
     this.calcularMedias();
     this.calcularStddev();
   }
   calcularMedias() {
-    if (this.columna1.length > 0) {
-      const sumaColumna1 = this.columna1.reduce((acc, valor) => acc + valor, 0);
-      this.mediaColumna1 = sumaColumna1 / this.columna1.length;
+    if (this.column1.length > 0) {
+      const sumaColumn1 = this.column1.reduce((acc, valor) => acc + valor, 0);
+      this.mediaColumn1 = sumaColumn1 / this.column1.length;
     }
-    if (this.columna2.length > 0) {
-      const sumaColumna2 = this.columna2.reduce((acc, valor) => acc + valor, 0);
-      this.mediaColumna2 = sumaColumna2 / this.columna2.length;
+    if (this.column2.length > 0) {
+      const sumaColumn2 = this.column2.reduce((acc, valor) => acc + valor, 0);
+      this.mediaColumn2 = sumaColumn2 / this.column2.length;
     }
   }
   calcularStddev() {
-    if (this.columna1.length > 0) {
-      const sumatoriaColumna1 = this.columna1.reduce((acc, valor) => {
-        const diff = valor - this.mediaColumna1;
+    if (this.column1.length > 0) {
+      const sumatoriaColumn1 = this.column1.reduce((acc, valor) => {
+        const diff = valor - this.mediaColumn1;
         return acc + diff ** 2;
       }, 0);
 
-      this.stddevColumna1 = Math.sqrt(
-        sumatoriaColumna1 / (this.columna1.length - 1),
+      this.stddevColumn1 = Math.sqrt(
+        sumatoriaColumn1 / (this.column1.length - 1),
       );
-      this.stddevColumna1 = this.redondearNumero(this.stddevColumna1, 2);
+      this.stddevColumn1 = this.closeNumber(this.stddevColumn1, 2);
     }
 
-    if (this.columna2.length > 0) {
-      const sumatoriaColumna2 = this.columna2.reduce((acc, valor) => {
-        const diff = valor - this.mediaColumna2;
+    if (this.column2.length > 0) {
+      const sumatoriaColumn2 = this.column2.reduce((acc, valor) => {
+        const diff = valor - this.mediaColumn2;
         return acc + diff ** 2;
       }, 0);
 
-      this.stddevColumna2 = Math.sqrt(
-        sumatoriaColumna2 / (this.columna2.length - 1),
+      this.stddevColumn2 = Math.sqrt(
+        sumatoriaColumn2 / (this.column2.length - 1),
       );
-      this.stddevColumna2 = this.redondearNumero(this.stddevColumna2, 2);
+      this.stddevColumn2 = this.closeNumber(this.stddevColumn2, 2);
     }
   }
 
-  redondearNumero(num: number, decimales: number): number {
+  closeNumber(num: number, decimales: number): number {
     const factor = Math.pow(10, decimales);
     return Math.round(num * factor) / factor;
   }
